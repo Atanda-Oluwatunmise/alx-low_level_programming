5ff2e9f08470e9e4019404f46660ce313e51a504
Atanda Oluwatunmise <atandatunmise>
2022-10-17 17:49:48 -0700

    numbers

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char low;
+for (char low = a;char low <= z; low++)
+     putchar(low);
+for (char low = A; char low <= Z; low++)
+     putchar(low);
+putchar(n);
+ 
+    return (0);
+}
