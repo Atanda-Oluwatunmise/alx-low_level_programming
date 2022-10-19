9a3cc0e2ea7c740304c9e0e135ea9c5976a9f9d3
Atanda Oluwatunmise <atandatunmise>
2022-10-19 15:34:56 -0700

    low level programming

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-         char *s = "_putchar"; /*arrays of characters or strings*/
-         int i = 0;
+	char *s = "_putchar"; /*arrays of characters or strings*/
+	int i = 0;
 
-         while (s[i] != 0)
-         {
-                _putchar(s[i]);
-                i++;
-         }
-         _putchar(n);
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
 
-         return (0);
+	return (0);
+
 }
