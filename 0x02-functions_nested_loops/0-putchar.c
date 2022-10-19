cbd12a1f5bd76365b8eba974e4f7ff50be217123
Atanda Oluwatunmise <atandatunmise>
2022-10-19 11:35:31 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,38 @@
 
-#include "holberton.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-        char *sh = "Holberton";
+#include "holberton.h"                                                                                                                                 
 
-        while (*sh)
-        {
-               _putchar(*sh);
-               sh++;
-        }
-        _putchar(n);
+/**                                                                                                                                                    
 
-        return (0);
+ * main - Entry point                                                                                                                                  
+
+ *                                                                                                                                                     
+
+ * Return: Always 0 (Success)                                                                                                                          
+
+ */                                                                                                                                                    
+
+int main(void)                                                                                                                                         
+
+{                                                                                                                                                      
+
+        char *sh = "Holberton";                                                                                                                        
+
+                                                                                                                                                       
+
+        while (*sh)                                                                                                                                    
+
+        {                                                                                                                                              
+
+               _putchar(*sh);                                                                                                                          
+
+               sh++;                                                                                                                                   
+
+        }                                                                                                                                              
+
+        _putchar(n);                                                                                                                                   
+
+                                                                                                                                                       
+
+        return (0);                                                                                                                                    
+
 }
